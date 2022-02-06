41eb402a1a5b98819084832a41e27b7ecdb59ee3
alishahzadsaab <alemhr1@gmail.com>
2022-02-06 23:48:16 +0500

    todo

--- esaint/models/doctype/ToDo/ToDo.js
+++ esaint/models/doctype/ToDo/ToDo.js
@@ -1,61 +1,57 @@
-const { indicators } = require('../../../../src/colors');
+import { indicators } from '../../../../src/colors';
 const { BLUE, GREEN } = indicators;
 
-module.exports = {
-  name: 'ToDo',
-  label: 'To Do',
-  naming: 'autoincrement',
-  isSingle: 0,
-  keywordFields: ['subject', 'description'],
-  titleField: 'subject',
-  indicators: {
-    key: 'status',
-    colors: {
-      Open: BLUE,
-      Closed: GREEN,
+export const name = 'ToDo';
+export const label = 'To Do';
+export const naming = 'autoincrement';
+export const isSingle = 0;
+export const keywordFields = ['subject', 'description'];
+export const titleField = 'subject';
+export const indicators = {
+  key: 'status',
+  colors: {
+    Open: BLUE,
+    Closed: GREEN,
+  },
+};
+export const fields = [
+  {
+    fieldname: 'subject',
+    label: 'Subject',
+    placeholder: 'Subject',
+    fieldtype: 'Data',
+    required: 1,
+  },
+  {
+    fieldname: 'status',
+    label: 'Status',
+    fieldtype: 'Select',
+    options: ['Open', 'Closed'],
+    default: 'Open',
+    required: 1,
+  },
+  {
+    fieldname: 'description',
+    label: 'Description',
+    fieldtype: 'Text',
+  },
+];
+export const quickEditFields = ['status', 'description'];
+export const actions = [
+  {
+    label: 'Close',
+    condition: (doc) => doc.status !== 'Closed',
+    action: async (doc) => {
+      await doc.set('status', 'Closed');
+      await doc.update();
     },
   },
-  fields: [
-    {
-      fieldname: 'subject',
-      label: 'Subject',
-      placeholder: 'Subject',
-      fieldtype: 'Data',
-      required: 1,
+  {
+    label: 'Re-Open',
+    condition: (doc) => doc.status !== 'Open',
+    action: async (doc) => {
+      await doc.set('status', 'Open');
+      await doc.update();
     },
-    {
-      fieldname: 'status',
-      label: 'Status',
-      fieldtype: 'Select',
-      options: ['Open', 'Closed'],
-      default: 'Open',
-      required: 1,
-    },
-    {
-      fieldname: 'description',
-      label: 'Description',
-      fieldtype: 'Text',
-    },
-  ],
-
-  quickEditFields: ['status', 'description'],
-
-  actions: [
-    {
-      label: 'Close',
-      condition: (doc) => doc.status !== 'Closed',
-      action: async (doc) => {
-        await doc.set('status', 'Closed');
-        await doc.update();
-      },
-    },
-    {
-      label: 'Re-Open',
-      condition: (doc) => doc.status !== 'Open',
-      action: async (doc) => {
-        await doc.set('status', 'Open');
-        await doc.update();
-      },
-    },
-  ],
-};
+  },
+];
